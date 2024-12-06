85e79ad3378eac8b7260250a364e26c6e2b11896
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-12-06 19:17:55 +0100

    Events

--- treveler/src/components/FavoritePlaces/FavoritePlaces.vue
+++ treveler/src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { isVNode } from 'vue'
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
+import IInput from '../IInput/IInput.vue'
 import IButton from '../IButton/IButton.vue'
 
 import { ref, reactive } from 'vue'
@@ -14,6 +15,11 @@ const changeButtonVariant = () => {
 <template>
   <div class="px-6">
     <div class="text-gray mb-4">Додані маркери ({{ counter }})</div>
+    <div class="py-5">
+      <IInput label="Some label" />
+      <a href="/" class="text-black" @click.prevent="() => console.log('prevented')">Click me</a>
+    </div>
+
     <FavoritePlace v-for="n in 4" :key="n" />
     <IButton class="w-full mt-10" :variant="buttonVariant" @click="changeButtonVariant">
       Додати маркер
---
+++ treveler/src/components/IInput/IInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  label: String,
+})
+
+const text = ref('')
+
+const changeValue = (event) => {
+  text.value = event.target.value
+}
+</script>
+
+<template>
+  <div class="w-full text-[#2c2c2c]">
+    <lable class="block">
+      <span class="block text-xs px-3 mb-2">{{ props.label }} : {{ text }}</span>
+      <input
+        class="w-full text-sm rounded-[4px] border-[#eaeaea] border-[1px] py-2 px3 focus:outline-primary"
+        type="text"
+        @input="changeValue"
+      />
+    </lable>
+  </div>
+</template>
