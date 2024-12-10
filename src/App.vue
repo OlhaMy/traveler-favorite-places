4bbb7367687102ce316199b8aa1c4b4415d52f96
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-12-10 16:15:52 +0100

    setup world map

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <script setup>
-import { ref } from 'vue'
-import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
-import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
-import createNewPlaceModal from './components/createNewPlaceModal/createNewPlaceModal.vue'
-// import HomepageView from './views/HomepageView.vue'
-// import FavoritePlacesVue from './components/FavoritePlaces/FavoritePlaces.vue'
-
-const isOpen = ref(true)
-
-const closeModal = () => {
-  isOpen.value = false
-}
-const openModal = () => {
-  isOpen.value = true
-}
+import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import FavoritePlacesVue from './components/FavoritePlaces/FavoritePlaces.vue'
+import { mapSettings } from './map/settings.js'
 </script>
 
 <template>
-  <button @click="openModal">Click me</button>
-  <!-- <HomepageView /> -->
-  <!-- <div class="bg-white h-screen w-[400px]">
-    <FavoritePlacesVue />
-  </div> -->
-  <RegistrationForm @submit="console.log" />
-  <LoginForm @submit="console.log" />
-
-  <createNewPlaceModal :is-open="isOpen" @close="closeModal" @submit="console.log" />
+  <main class="flex h-screen">
+    <div class="bg-white h-screen w-[400px] shrink-0 overflow-auto pb-10">
+      <FavoritePlacesVue />
+    </div>
+    <div class="w-full h-full flex items-center justify-center text-6xl">
+      <MapboxMap
+        class="w-full h-full"
+        :center="[30.523333, 50.450001]"
+        :zoom="10"
+        :access-token="mapSettings.apiToken"
+        :map-style="mapSettings.style"
+      ></MapboxMap>
+    </div>
+  </main>
 </template>
--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,28 +1,16 @@
 <script setup>
-import { isVNode } from 'vue'
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
-import IInput from '../IInput/IInput.vue'
 import IButton from '../IButton/IButton.vue'
-
-import { ref, reactive } from 'vue'
-const buttonVariant = ref('gradient')
-
-const changeButtonVariant = () => {
-  buttonVariant.value = buttonVariant.value === 'gradient' ? 'outlined' : 'gradient'
-}
 </script>
 
 <template>
   <div class="px-6">
     <div class="text-gray mb-4">Додані маркери ({{ counter }})</div>
-    <div class="py-5">
-      <IInput label="Some label" />
-      <a href="/" class="text-black" @click.prevent="() => console.log('prevented')">Click me</a>
-    </div>
 
-    <FavoritePlace v-for="n in 4" :key="n" />
-    <IButton class="w-full mt-10" :variant="buttonVariant" @click="changeButtonVariant">
-      Додати маркер
-    </IButton>
+    <slot name="label"></slot>
+    <slot name="list"><FavoritePlace v-for="n in 4" :key="n" /></slot>
+    <slot></slot>
+
+    <IButton class="w-full mt-10" variant="gradient"> Додати маркер </IButton>
   </div>
 </template>
